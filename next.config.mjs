2ccd51adc66b03b9bf6c7091c28f45fce82b211f
Bill-d77 <bilal.daw@gmail.com>
2025-08-10 11:07:41 +0300

    fix(image): allow tobitengineers.com in Next Image remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "www.tobitengineers.com",
+      },
+      {
+        protocol: "https",
+        hostname: "tobitengineers.com",
+      },
     ],
   },
 };
